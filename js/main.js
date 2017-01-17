54688719531b9496fb006f4670f4b94bc5357573
maribel <you@example.com>
2017-01-17 15:02:09 -0500

    fin de bici locas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+function validateEmail(_evt){
+    
+      var email = document.getElementById("input-email").value;
+      var mensaje = document.getElementById("mensaje3");
+      
+      if(!/([a-zA-Z0-9(-_.)]+[@][a-zA-Z0-9]+[.][a-zA-Z]+)/g.test(email)){
+        mensaje.innerHTML = "<h5 style='color:#8A2BE2';>Correo Electrónico Inválido</h5>"
+      } else {
+        mensaje.innerHTML = "";
+      }
+      
+} 
+ function validatePass(_evt){
+      
+      var pass = document.getElementById("input-password").value;
+      var msg = document.getElementById("mensaje");
+      
+      
+      if((pass != "123456" || pass != "098754" || pass.toLowerCase() != "password") && (pass.length < 6) ) {
+       msg.innerHTML = "<h5 style='color:#8A2BE2';>Contraseña Inválida</h5>"
+      } else {
+       msg.innerHTML = "";
+      }
+        
+  }
+    
+
+  
